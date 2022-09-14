c283bfc8987526a257c0df8414ded919498fed9c
id0ntmind <n0thingss@yandex.ru>
2022-09-14 11:50:12 +0300

    add hooks | app update 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import Footer  from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import Card from './components/Card/Card'
+import { TextContextProvider } from './context/textContext';
+import { ImgContextProvider } from './context/imgContext';
+
+
+const wrapper = {
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100vh',
+};
 
 const App = ()  => {
   return (
-    <div>
-      <Header />
-      
-      <Footer />
+    <div style={wrapper}>
+      <ImgContextProvider>
+        <TextContextProvider>
+          <Header />
+          <Card />
+          <Footer />
+        </TextContextProvider>
+      </ImgContextProvider>
     </div>
   );
 }
